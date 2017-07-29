6f5a82313b5acfafd6718ebff4116e43581d8373
Amy7 <Amyquan@mailinator.com>
2017-07-29 10:57:34 -0400

    Add files via upload

---
+++ dogage.py
@@ -0,0 +1,12 @@
+
+month = int(input("input months:"))
+year = int (input("input years:"))
+humenmonth = 7*month
+humenyear = 7*year + (humenmonth//12)
+print("the dog years is about",humenyear)
+
+
+
+
+
+
