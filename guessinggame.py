327ca643647cddcd9551fc6481b7b4a06aa51ca4
Amy7 <Amyquan@mailinator.com>
2017-07-29 14:58:18 -0400

    Add files via upload

--- guessinggame.py
+++ guessinggame.py
@@ -1,24 +1,23 @@
 import random
 number = random.randint(1,10)
-print(number)
+#print(number)
 n = 3
 
 while(n):
     try:
         guess = int(input(("please guess the number:")))
         while(guess > 10 or guess < 0):
-            guess = int(input((" please input your number from 1 to 10")))
+            guess = int(input(("please input your number from 1 to 10")))
         n -= 1
         if(guess == number):
             print("congratulation!")
+            break
         elif(guess-number==1 or guess-number==-1):
             print("it's hot")
         elif(guess-number==2 or guess-number==-2):
             print("it's warm")
-
-            print("This is the except: please input your number from 1 to 10")
         else:
             print("it's cold")
     except:
-        print("This is the except: please input your number from 1 to 10")
+        print("please input your number from 1 to 10")
 
