e75d90460012cdf856e710a54e2e64d907126925
Amy7 <Amyquan@mailinator.com>
2017-07-29 13:12:59 -0400

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,10 @@
+
+
+for num in range(2,200):
+    for i in range(2, num):
+        if(num%i == 0):
+            break
+    else:
+        print(num)
+
+
---
+++ quadratic.py
@@ -0,0 +1,9 @@
+
+a = int(input("please input number a:"))
+b = int(input("please input number b:"))
+c = int(input("please input number c:"))
+x = int(input("please input number x:"))
+y = a*(x**2)+ (b*x) + c
+
+print("the following quadratic was entered: {}x^2 +({})x +({})".format(a,b,c))
+print("the value of the quadratic is:",y)
