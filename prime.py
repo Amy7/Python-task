2475ccbfd5d8944155b24027cca0d3376a2acad1
Amy7 <Amyquan@mailinator.com>
2017-07-29 13:18:36 -0400

    Add files via upload

--- prime.py
+++ prime.py
@@ -1,10 +1,10 @@
 
 
-for num in range(2,200):
+for num in range(2,100):
     for i in range(2, num):
         if(num%i == 0):
             break
     else:
-        print(num)
+        print(num,"is prime number")
 
 
